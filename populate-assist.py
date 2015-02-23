b9073d3a67b094adf0d269b0d760a4560575d890
Etienne Perot <etienne@perot.me>
2015-02-22 16:31:06 -0800

    Assist with artwork data entry.

--- populate-assist.py
+++ populate-assist.py
@@ -1,50 +1,100 @@
 #!/usr/bin/env python3
 
 import sys
+import threading
 import time
 import webbrowser
 import data
 
-def EnsureAvailableCallback(context, d, sourceClass):
+def Search(sourceClass, terms):
+	webbrowser.open_new_tab(sourceClass.SearchURL(terms))
+	time.sleep(1) # Necessary to makesure web pages open in a consistent order.
+
+def Ask(context, prompt, ifEmpty='None'):
+	print('-' * 8)
+	return str(input('%s: %s: ' % (context, prompt)) or ifEmpty)
+
+def EnsureAvailableCallback(context, sourceClass):
 	source = context.GetSource(sourceClass)
 	if source:
 		return lambda: None
 	print('%s: Unknown value for "%s". Opening search page.' % (context, sourceClass.KEY))
-	webbrowser.open_new_tab(sourceClass.SearchURL(context.name_searchable))
-	time.sleep(1) # Necessary to makesure web pages open in a consistent order.
+	Search(sourceClass, context.name_searchable)
+	default = []
+	def backgroundFetch():
+		try:
+			default.append(sourceClass.GetBestMatch(context.name_searchable))
+		except Exception as e:
+			print('Exception while retrieving best match for %s: %s' % (context, e))
+			default.append(None)
+	threading.Thread(target=backgroundFetch).start()
 	def callback():
-		value = input('%s: Value for "%s": ' % (context, sourceClass.KEY)) or 'None'
+		while not default:
+			time.sleep(.1)
+		value = Ask(context, 'Value for "%s" ("None" for None, default="%s")' % (sourceClass.KEY, default[0]), ifEmpty=default[0])
 		if value.startswith('http://') or value.startswith('https://'):
-			value = sourceClass.ParseOpenURLToID(value)
+			value = sourceClass.ParseOpenURLToID(sourceClass.CleanURL(value))
 		if value is not None and value.isdigit():
 			value = int(value)
-		d[sourceClass.KEY] = value
+		context.kind_data[sourceClass.KEY] = value
 	return callback
 
-def PopulateAssist(context):
-	sources = []
-	if context.kind == data.Context.KIND_SERIES:
-		sources.append((context.series, data.TVDB))
-	elif context.kind == data.Context.KIND_SEASON:
-		sources.append((context.season, data.AniDB))
-		sources.append((context.season, data.MAL))
-		sources.append((context.season, data.IMDB))
-	elif context.kind == data.Context.KIND_MOVIE:
-		sources.append((context.movie, data.AniDB))
-		sources.append((context.movie, data.MAL))
-		sources.append((context.movie, data.IMDB))
-	elif context.kind == data.Context.KIND_OVA:
-		sources.append((context.ova, data.AniDB))
-		sources.append((context.ova, data.MAL))
+def PopulateAssistIDs(context):
+	sources = {
+		data.Context.KIND_SERIES: (data.TVDB,),
+		data.Context.KIND_SEASON: (data.AniDB, data.MAL, data.IMDB),
+		data.Context.KIND_MOVIE: (data.AniDB, data.MAL, data.IMDB),
+		data.Context.KIND_OVA: (data.AniDB, data.MAL),
+		data.Context.KIND_SOUNDTRACK: (),
+	}[context.kind]
 	callbacks = []
-	for d, sourceClass in sources:
-		callbacks.append(EnsureAvailableCallback(context, d, sourceClass))
+	for sourceClass in sources:
+		callbacks.append(EnsureAvailableCallback(context, sourceClass))
 	for callback in callbacks:
 		callback()
+	context.Overwrite()
+
+def PopulateAssistArt(context):
+	missing = []
+	sources = set()
+	need = {
+		data.Context.KIND_SERIES: ('background', 'banner', 'poster'),
+		data.Context.KIND_SEASON: ('background', 'banner', 'poster'),
+		data.Context.KIND_MOVIE: ('background', 'poster'),
+		data.Context.KIND_OVA: ('background', 'poster'),
+		data.Context.KIND_SOUNDTRACK: (),
+	}[context.kind]
+	for art in need:
+		if context.GetSingle(art):
+			continue
+		missing.append(art)
+		for sourceClass in (data.TVDB, data.MoviePosterDB, data.ZeroChan, data.MiniTokyo):
+			if art in sourceClass.ART_RESOURCE_TYPES:
+				sources.add(sourceClass)
+	if not missing:
+		return
+	assert sources
+	for sourceClass in sources:
+		Search(sourceClass, context.name_searchable)
+	if data.MoviePosterDB in sources:
+		# MoviePosterDB also supports searching by IMDB ID.
+		imdbs = []
+		for sub in context.GatherSubContexts():
+			imdb = sub.Get('imdb')
+			if imdb and imdb not in imdbs:
+				imdbs.append(imdb)
+		for imdb in sorted(imdbs):
+			Search(data.MoviePosterDB, imdb)
+	for art in missing:
+		url = Ask(context, 'URL for "%s"' % (art,))
+		for sourceClass in sources:
+			url = sourceClass.CleanURL(url)
+		context.kind_data[art] = url
 	context.Overwrite()
 
 if __name__ == '__main__':
 	for path in sys.argv[1:]:
 		for context in data.Traverse(path):
 			print('Processing:', context)
-			PopulateAssist(context)
+			PopulateAssistIDs(context)
+			#PopulateAssistArt(context)
